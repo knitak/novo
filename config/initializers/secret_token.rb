2a054f4645598d77708f74413a1ba033e25cb040
Ana Pedrosa <anacpedrosa@gmail.com>
2012-11-01 20:17:16 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Novo::Application.config.secret_token = '2f13a58c2c874771f9981276b19350f9e6e22225da268fe7cea2df46e1971d5ee51553030950661a8f1702b9b589520e5dd0a02d811f251b5e6eea7ea0ea8244'
